5e0bbfbac1654ef99deceb2dc79eab6c29403cc2
wm <wm>
2022-05-06 09:30:19 +0800

    changed + operator to append

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -1,41 +1,37 @@
-#!/usr/bin/env python
-
-################################################################################
-# @Author Wei-Ming Chen, PhD                                                   #
-# @200. Number of Islands                                                      #
-# @version: v1.0.0                                                             #
-################################################################################
-
 '''
-URL: https://leetcode.com/problems/number-of-islands/
+200. Number of Islands
+@auth: Wei-Ming Chen, PhD
+@date: 2022/05/01
+@update: 2022/05/06
+@description:
 
-Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
-Example 1:
-    Input: grid = [
-    ["1","1","1","1","0"],
-    ["1","1","0","1","0"],
-    ["1","1","0","0","0"],
-    ["0","0","0","0","0"]
-    ]
-    Output: 1
+    Example 1:
+        Input: grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+        ]
+        Output: 1
 
-Example 2:
-    Input: grid = [
-    ["1","1","0","0","0"],
-    ["1","1","0","0","0"],
-    ["0","0","1","0","0"],
-    ["0","0","0","1","1"]
-    ]
-    Output: 3
+    Example 2:
+        Input: grid = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+        ]
+        Output: 3
 
-Constraints:
-    m == grid.length
-    n == grid[i].length
-    1 <= m, n <= 300
-    grid[i][j] is '0' or '1'.
+    Constraints:
+        m == grid.length
+        n == grid[i].length
+        1 <= m, n <= 300
+        grid[i][j] is '0' or '1'.
 '''
 
 import time
@@ -58,7 +54,7 @@ class Solution:
                 for dr, dc in dirs:
                     nr, nc = r+dr, c+dc
                     if nr >= 0 and nc >= 0 and nr < h and nc < w:
-                        if grid[nr][nc] == "1": q += [[nr,nc]]
+                        if grid[nr][nc] == "1": q.append([nr,nc])
             return 1
 
         h, w = len(grid), len(grid[0])
