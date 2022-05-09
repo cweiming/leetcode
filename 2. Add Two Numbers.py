66ff7a4f2e17b58dbd8ae05472ee79cf8e7cb921
wm <wm>
2022-05-09 10:32:02 +0800

    Improved ListNode method

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -2,7 +2,7 @@
 2. Add Two Numbers
 @auth: Wei-Ming Chen, PhD
 @date: 2022/05/06
-@update: 2022/05/06
+@update: 2022/05/09
 @description:
 
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
@@ -44,67 +44,51 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(l1, l2):
-        b1, b2 = [l1.val], [l2.val]
-        while l1.next is not None:
-            l1 = l1.next
-            b1.append(l1.val)
-        while l2.next is not None:
-            l2 = l2.next
-            b2.append(l2.val)
-
-        b1_len, b2_len = len(b1), len(b2)
-        if b1_len > b2_len:
-            b2 += [0 for x in range(b1_len-b2_len)]
-        if b2_len > b1_len:
-            b1 += [0 for x in range(b2_len-b1_len)]
-        b3 = [[b1.pop(),b2.pop()] for i in range(len(b1))]
-
         sv = 0; # stored value
-        l3 = ListNode(next = ListNode())
-        tmp = l3.next
-        i = 0;
+        curr = ListNode(next = ListNode())
+        ptr = curr.next
+        while l1 or l2:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        while b3:
-            v1,v2 = b3.pop()
             if v1+v2+sv <= 9:
-                tmp.val = v1+v2+sv
+                ptr.val = v1+v2+sv
                 sv = 0
             if v1+v2+sv >= 10:
-                tmp.val = v1+v2+sv-10
+                ptr.val = v1+v2+sv-10
                 sv = 1
-            if b3:
-                tmp.next = ListNode()
-                tmp = tmp.next
-                i += 1
 
-        if sv: tmp.next = ListNode(sv)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            if l1 or l2:
+                ptr.next = ListNode()
+                ptr = ptr.next
+            else:
+                break
+        if sv: ptr.next = ListNode(sv)
 
-        a = []
-        l3 = l3.next
-        while l3.next is not None:
-            a += [l3.val]
-            l3 = l3.next
-        print(a + [l3.val])
+        return curr.next
 
-        return l3.next
 
 if __name__ == '__main__':
-    # Runtime: faster than 43.55% of Python3 online submissions
-    # Memory Usage: less than 10.51% of Python3 online submissions
+    # Runtime: faster than 70.12% of Python3 online submissions
+    # Memory Usage: less than 99.44% of Python3 online submissions
 
     # l1 = ListNode(2,ListNode(4,ListNode(3,None))) # [2,4,3]
     # l2 = ListNode(5,ListNode(6,ListNode(4))) # [5,6,4]
+    # output [7, 0, 8]
 
     # l1 = ListNode(0) # [0]
     # l2 = ListNode(0) # [0]
+    # output [0]
 
     # l1 = ListNode(9,ListNode(9,ListNode(9,ListNode(9,ListNode(9,ListNode(9,ListNode(9))))))) #[9,9,9,9,9,9,9]
     # l2 = ListNode(9,ListNode(9,ListNode(9,ListNode(9)))) #[9,9,9,9]
-    # l1 = [9,9,9,9,9,9,9]
-    # l2 = [9,9,9,9]
+    # output [8, 9, 9, 9, 0, 0, 0, 1]
 
-    # l1 = ListNode(2,ListNode(4,ListNode(9))) # [2,4,9]
-    # l2 = ListNode(5,ListNode(6,ListNode(4,ListNode(9)))) # [5,6,4,9]
+    l1 = ListNode(2,ListNode(4,ListNode(9))) # [2,4,9]
+    l2 = ListNode(5,ListNode(6,ListNode(4,ListNode(9)))) # [5,6,4,9]
+    # output [7,0,4,0,1]
 
     start_time = time.time()
     print("Output:", Solution.addTwoNumbers(l1, l2))
