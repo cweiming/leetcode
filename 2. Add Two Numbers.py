917055530d6aa7cb6426d3e412954947c20df5fb
wm <wm>
2022-05-11 17:25:44 +0800

    Improved 2. Add Two Numbers

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -2,7 +2,7 @@
 2. Add Two Numbers
 @auth: Wei-Ming Chen, PhD
 @date: 2022/05/06
-@update: 2022/05/09
+@update: 2022/05/11
 @description:
 
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
