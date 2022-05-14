a624d5ff9aba50c007c7fc6276ca7373f43b03ea
Your Name <you@example.com>
2022-05-14 13:33:40 +0800

    20. Valid Parentheses

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,69 @@
+'''
+20. Valid Parentheses
+@auth: Wei-Ming Chen, PhD
+@date: 2022/05/14
+@update: 2022/05/14
+@description:
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    
+
+    Example 1:
+
+    Input: s = "()"
+    Output: true
+    Example 2:
+
+    Input: s = "()[]{}"
+    Output: true
+    Example 3:
+
+    Input: s = "(]"
+    Output: false
+    
+
+    Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+'''
+
+import time
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bk, sk = {"(":")", "{":"}", "[":"]"}, []
+        for v in s:
+            if sk and sk[-1] in bk and v == bk[sk[-1]]: sk.pop()
+            else: sk.append(v)
+        return sk == []
+
+if __name__ == '__main__':
+    # Runtime: faster than 75.69% of Python3 online submissions
+    # Memory Usage: less than 70.91% of Python3 online submissions
+
+    # s = "()"
+    # Output: true
+
+    # s = "()[]{}"
+    # Output: true
+
+    # s = "(]"
+    # Output: false
+
+    # s = "(){}}{"
+    # Output: false
+    
+    # s = "[()[(){}]]"
+    # Output: true
+
+    s = "[()[([){]}]]"
+    # Output: false
+
+    start_time = time.time()
+    print("Output:", Solution().isValid(s))
+    print("Runtime: %6f sec:" % (time.time() - start_time))
