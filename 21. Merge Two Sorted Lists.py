1ccb5d9c4fd1f9d4d0edbcc7395e5771bddf4100
Your Name <you@example.com>
2022-05-15 19:00:50 +0800

    21. Merge Two Sorted Lists

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -43,7 +43,6 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
-        if not list1 and not list2: return None
         if not list1: return list2
         if not list2: return list1
 
@@ -68,14 +67,14 @@ class Solution:
 
 if __name__ == '__main__':
     # Runtime: faster than 76.02% of Python3 online submissions
-    # Memory Usage: less than 33.02% of Python3 online submissions
+    # Memory Usage: less than 79.89% of Python3 online submissions
 
     list1 = ListNode(1,ListNode(2,ListNode(4))) # [1,2,4]
     llis2 = ListNode(1,ListNode(3,ListNode(4))) # [1,3,4]
     # Output: [1,1,2,3,4,4]
 
-    # list1 = ListNode() # []
-    # llis2 = ListNode() # []
+    list1 = None # []
+    llis2 = None # []
     # Output: []
 
     # list1 = None # []
